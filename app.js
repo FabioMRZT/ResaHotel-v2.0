a54c6bfd679f041711a80edc5be4d7fd6bb21967
Fabio <fabio38450@gmail.com>
2025-10-01 11:03:46 +0200

    app.js

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const path = require('path');
-
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// Navbar de base
-app.get('/', (req, res) => res.redirect('/clients'));
-
-app.use('/clients', require('./routes/clients'));
-app.use('/reservations', require('./routes/reservations'));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Serveur http://localhost:${PORT}`));
-=======
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -58,4 +38,3 @@ app.use((req, res) => {
 app.listen(PORT, () => {
  console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
->>>>>>> e8ae49397901807e0adff4e87aa82c900c7fed2f
